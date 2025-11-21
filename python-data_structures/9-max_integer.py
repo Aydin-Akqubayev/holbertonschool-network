8007c270e75f057a8ba99f71164279366aa4025e
Aydin-Akqubayev <aydinakqubayev1@gmail.com>
2025-11-21 20:15:06 +0400

    Correction of 9. Find the max

--- python-data_structures/9-max_integer.py
+++ python-data_structures/9-max_integer.py
@@ -1,3 +1,14 @@
 #!/usr/bin/python3
+def compare(a, b):
+    if a < b:
+        res = b
+    else:
+        res = a
+    return res
+
+
 def max_integer(my_list):
-    return max(my_list)
+    a = 0
+    for i in my_list:
+        a = compare(a, i)
+    return a
