c513b91776feb80ec08143bda23c37ad354535d5
Aydin-Akqubayev <aydinakqubayev1@gmail.com>
2025-11-28 20:07:55 +0400

    new

---
+++ python-input_output/1-write_file.py
@@ -0,0 +1,11 @@
+#!/usr/bin/python3
+'''This code add text to file'''
+
+def write_file(file, text):
+    '''Practice with open function'''
+
+    with open(file, 'w') as f:
+
+        f.write(text)
+
+    return len(text)
